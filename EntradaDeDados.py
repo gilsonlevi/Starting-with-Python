04eb22a427b05dcc18027d89fc5cf2ef0778a0cd
gilsonlevi <gilsonlevi328@gmail.com>
2023-06-17 12:05:23 -0300

    Initial commit

---
+++ EntradaDeDados.py
@@ -0,0 +1,5 @@
+temperaturaF = input("Digite uma temperatura em Fahrenheit: ")
+
+temperaturaC = (temperaturaF - 32) * 5 / 9
+
+print(int(temperaturaC))
---
+++ PrimeiroPrograma.py
@@ -0,0 +1,16 @@
+#type
+#Declara o tipo de um valor ou variavel
+print(type(10))
+nome = "Pedro"
+print(type(nome))
+
+#Converter um float para int
+numero1 = 23.333
+numero2 = int(numero1)
+print(numero2)
+
+#Tamano de uma string
+texto = "Bom dia"
+print("Tamanho do texto: " ,  len(texto))
+
+print(2*1**2)

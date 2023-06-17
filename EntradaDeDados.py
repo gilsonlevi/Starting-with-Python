3522a52461ce2fd737d574752c815b44129592b7
gilsonlevi <gilsonlevi328@gmail.com>
2023-06-17 13:34:30 -0300

    Entrada de Dados

--- EntradaDeDados.py
+++ EntradaDeDados.py
@@ -1,5 +1,14 @@
-temperaturaF = input("Digite uma temperatura em Fahrenheit: ")
+# temperaturaF = input("Digite uma temperatura em Fahrenheit: ")
 
-temperaturaC = (temperaturaF - 32) * 5 / 9
 
-print(int(temperaturaC))
+# temperaturaC = (float(temperaturaF) - 32) * 5 / 9
+
+# print("A temperatura em Fahrenheit " , temperaturaF, " em Celsius é igual a:" ,  int(temperaturaC))
+a = int(input("A"))
+b = int(input("B"))
+
+aux = a
+a = b 
+b =aux
+print(a)
+print(b)
---
+++ OiParaVoce.py
@@ -0,0 +1,5 @@
+nomeDaMae = input("Qual o nome da sua mãe: ")
+nomeDaPai = input("Qual o nome do seu pai: ")
+
+print("Bom dia Sra.", nomeDaMae, "!!! e bom dia Sr.", nomeDaPai)
+

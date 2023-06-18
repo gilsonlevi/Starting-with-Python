12a46ad9fe16d7cbdb819d83cf8069a3216080de
gilsonlevi <gilsonlevi328@gmail.com>
2023-06-18 16:38:31 -0300

    Exercicio de funções

---
+++ Tarefa de Programação - 4/maximo.py
@@ -0,0 +1,7 @@
+def maximo(x,y):
+    max = 0
+    if (x > y):
+        max = x
+    else:
+        max = y
+    return max
---
+++ Tarefa de Programação - 4/vogais.py
@@ -0,0 +1,6 @@
+def vogal(texto):
+    if (texto == "a" or texto == "e" or texto == "i" or texto == "o" or texto == "u" or texto == "A" or texto == "E" or texto == "I" or texto == "O" or texto == "U"):
+        return True
+    else:
+        return False
+

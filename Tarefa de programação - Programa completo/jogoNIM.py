c54b6634efe670cea8e3fa4a08856711b875bd73
gilsonlevi <gilsonlevi328@gmail.com>
2023-06-19 09:51:35 -0300

    Començando projeto NIM

---
+++ Tarefa de programação - Programa completo/jogoNIM.py
@@ -0,0 +1,32 @@
+def main():
+    while True:
+        print("Bem vindo ao jogo NIM! Escolha:")
+        print("1 - para jogar uma partida isolada")
+        print("2 - para jogar um campeonato")
+        n = int(input())
+        if(n == 1):
+            partida()
+        elif (n == 2):
+            campeonato()
+        else:
+            print("Resposta invalida")
+        
+        
+
+def partida():
+    print("Você escolheu uma partida isolada!")
+    print("")
+    pecas = int(input("Quantas peças? "))
+    limite = int(input("Limite de peças por jogada? "))
+
+def computador_escolhe_jogada(n,m):
+    print
+
+def campeonato():
+    print("Campeonato")
+
+
+main()
+    
+    
+    
